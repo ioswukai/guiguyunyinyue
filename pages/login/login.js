3f23afffe9646ba3bb1a0f82f9f73653a2254158
kai Wu <ioswukai@163.com>
2022-04-01 22:02:02 +0800

    提交登录页面

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,12 +1,12 @@
-// pages/login/login.js
+import request from '../../utils/request'
 Page({
-
   /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
+     * 页面的初始数据
+     */
+    data: {
+        phone: '', // 手机号
+        password: '' // 用户密码
+    },
 
   /**
    * 生命周期函数--监听页面加载
@@ -15,6 +15,90 @@ Page({
 
   },
 
+  // 表单项内容发生改变的回调
+  handleInput(event) {
+    // let type = event.currentTarget.id;// id传值 取值： phone || password
+    let type = event.currentTarget.dataset.type; // data-key=value
+    // console.log(event);
+    this.setData({
+      // 对象里面操作属性，如果属性名是变量，需要使用[]
+      [type]: event.detail.value
+    })
+  },
+  
+  
+  // 登录的回调
+  async login(){
+    // 1. 收集表单项数据
+    let {phone, password} = this.data;
+    // 2. 前端验证
+    /*
+    * 手机号验证：
+    *   1. 内容为空
+    *   2. 手机号格式不正确
+    *   3. 手机号格式正确，验证通过
+    * */
+    
+    if(!phone){
+      // 提示用户
+      wx.showToast({
+        title: '手机号不能为空',
+        icon: 'none'
+      })
+      return;
+    }
+    // 定义正则表达式
+    let phoneReg = /^1(3|4|5|6|7|8|9)\d{9}$/;
+    if(!phoneReg.test(phone)){
+      wx.showToast({
+        title: '手机号格式错误',
+        icon: 'none'
+      })
+      return;
+    }
+    
+    if(!password){
+      wx.showToast({
+        title: '密码不能为空',
+        icon: 'none'
+      })
+      return;
+    }
+  
+    // 后端验证
+    const data = {phone, password, isLogin: true}
+    let result = await request('/login/cellphone', {data})
+    if(result.code === 200) { // 登录成功
+      wx.showToast({
+        title: '登录成功'
+      })
+      debugger
+      // 将用户的信息存储至本地
+      wx.setStorageSync('userInfo', JSON.stringify(result.profile))
+      
+      
+      // 跳转至个人中心personal页面
+      wx.reLaunch({
+        url: '/pages/personal/personal'
+      })
+    }else if(result.code === 400){
+      wx.showToast({
+        title: '手机号错误',
+        icon: 'none'
+      })
+    }else if(result.code === 502){
+      wx.showToast({
+        title: '密码错误',
+        icon: 'none'
+      })
+    }else {
+      wx.showToast({
+        title: '登录失败，请重新登录',
+        icon: 'none'
+      })
+    }
+  },
+  
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
