3bb64f83791d6401930cad07859a670543c3934b
kai Wu <ioswukai@163.com>
2022-04-02 18:32:15 +0800

    VideoContext  控制同时只能有1首歌播放

--- pages/video/video.js
+++ pages/video/video.js
@@ -10,6 +10,7 @@ Page({
     videoGroupList: [], // 导航的标签数据
     navId: 0, // 选中导航的标识
     videoList: [], // 导航对应的视频列表
+    vid: null, // 前一个视频的id
   },
 
   /**
@@ -66,6 +67,40 @@ Page({
     })
     // 获取导航对应视频列表数据
     this.getVideoListData(navId);
+  },
+
+  /* 开始/继续播放 */
+  handlePlay(event) {
+      /*
+      问题： 多个视频同时播放的问题
+    * 需求：
+    *   1. 在点击播放的事件中需要找到上一个播放的视频
+    *   2. 在播放新的视频之前关闭上一个正在播放的视频
+    * 关键：
+    *   1. 如何找到上一个视频的实例对象
+    *   2. 如何确认点击播放的视频和正在播放的视频不是同一个视频
+    * 单例模式：
+    *   1. 需要创建多个对象的场景下，通过一个变量接收，始终保持只有一个对象，
+    *   2. 节省内存空间
+    * */
+
+    let vid = event.currentTarget.id;
+    if (this.data.vid) {
+    // 存在之前的vid
+    if (this.data.vid !== vid) {
+      // 点击不同的视频，则关闭之前的视频
+      this.videoContext.stop();
+    } else {
+      // 点击相同的视频，则是继续播放，下面逻辑无需处理
+      return;
+    }
+  }
+    // 创建 新的控制video标签的实例对象
+    this.videoContext = wx.createVideoContext(vid);
+    this.setData({
+      vid
+    })
+    console.log(vid, videoContext, this.videoContext)
   },
 
   /**
