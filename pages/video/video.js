78b142b7c230f8abc3de98eaa8c5163759db42d2
kai Wu <ioswukai@163.com>
2022-04-02 13:55:27 +0800

    搭建视频导航

--- pages/video/video.js
+++ pages/video/video.js
@@ -1,18 +1,43 @@
 // pages/video/video.js
+
+import request from "../../utils/request"
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    videoGroupList: [], // 导航的标签数据
+    navId: 0, // 选中导航的标识
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // 获取导航的标签数据
+    this.getVideoGroupData()
+  },
 
+  /* 获取导航的标签数据 */
+  async getVideoGroupData() {
+    const videoGroupListData = await request('/video/group/list')
+    this.setData({
+      videoGroupList: videoGroupListData.data.slice(0, 14),
+      navId: videoGroupListData.data[0].id
+    })
+  },
+
+  /* 切换导航的回调 */
+  changeNav(event) {
+    // 通过`id`向`event`传参的时候如果传的是`number`**会自动转换成**`string`
+    // const navId = event.currentTarget.id;
+    // this.setData({
+    //   navId: navId*1
+    // })
+    // 通过`data-key=value`向`event`传参的时候如果传的是`number`**它并不会自动转换成**`string`
+    const navId = event.currentTarget.dataset.id;
+    this.setData({navId})
   },
 
   /**
